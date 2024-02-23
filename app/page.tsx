95d6172cfdb43f53eb9cb1479beb143f1e0122c5
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-23 02:15:55 -0600

    finished the hero section. created box for location, distance and elevation. formatted layout and page files.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
-import Hero from "@/components/Hero"
-
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
