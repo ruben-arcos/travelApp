8b9bf28b9e49469053b9bf1b8e408155f384d9e0
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-25 01:25:54 -0600

    started working on the features section. Started by placing background image on section, as well as image of iphone when screen is large. Then, I created the main title, and added icons for each feature as well as its titles.

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { FEATURES } from "@/constants";
+import Image from "next/image";
+import React from "react";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    // overflow-hidden to avoid scrollbar
+    // bg-feature-bg to apply background img
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+        {/*
+         * flex-col - so they appear one on top of the other on mobile devices
+         * features section will take 60% on large devices
+         */}
+        <div className="-z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="capitalize bold-40 lg:bold-64">our features</h2>
+          </div>
+          <ul>
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Features
+type FeatureItem = {
+  title: string;
+  icon: string;
+  description: string;
+};
+
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} alt="map" width={28} height={28} />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      ,
+    </li>
+  );
+};
+
+export default Features;
