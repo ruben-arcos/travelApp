14c08bf08f0446c21edcb293244d476ebe37bb28
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-25 02:20:35 -0600

    created the title as well as the buttons to download app. L side of full length app done.

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const GetApp = () => {
   return (
-    <section className='flexCenter w-full flex-col pb-[100px]'>
-      <div className='get-app'>
-        
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      {/* gep-app gives the dark background to this section */}
+      <div className="get-app">
+        {/*
+         * L side of app on bigger screens, or top if on mobile
+         * z-20 appears on top
+         * flex-1 so it expands naturally
+         */}
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            get for free now
+          </h2>
+          <p className="regular-16 text-gray-10">Availabe on iOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row ">
+            <Button 
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+             <Button 
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default GetApp
+export default GetApp;
