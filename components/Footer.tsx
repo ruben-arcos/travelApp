693d481ea0e8b5942ae2ecf7d0bc183ea519b76a
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-25 03:07:32 -0600

    created the links for learn more, our community and contact us

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -10,9 +11,55 @@ const Footer = () => {
           <Link href="/" className="mb-10">
             <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
+
+          <div className="flex flex-wrap gap-10 sm:justify-between m:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    // to add a different path, modify here
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    {/* admin and email: */}
+                    <p className="whitespace-nowrap">{link.label}:</p>
+                    {/* numbers and emails */}
+                    <p className="medium-14 whitespace-nowrap text-blue-70">{link.value}</p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
+  );
+};
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
   );
 };
 
