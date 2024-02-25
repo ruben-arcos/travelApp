d71d7de59545d5ab7934c951ed2ea062cb8d695d
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-24 23:10:22 -0600

    started working on the guide file. added camp image, created the h2, and also added text to it together with the p tag.

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Guide = () => {
   return (
-    <div>Guide</div>
-  )
-}
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          we are here for you
+        </p>
 
-export default Guide
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="capitalize bold-40 lg:bold-64 xl:max-w-[390px]">
+            guide you to easy path
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Only with the hilink application you will no longer get lost and get
+            lost again, because we already support offline maps when there is no
+            internet connection in the field. Invite your friends, relatives and
+            friends to have fun in the wilderness through the valley and reach
+            the top of the mountain
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Guide;
