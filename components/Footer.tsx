6c67563675e573175cc26bc4fde1aa69628db909
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-25 02:32:37 -0600

    started working with the footer file.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    < footer className="flexCenter mb-24">
+    </footer>
   )
-}
+};
 
-export default Footer
+export default Footer;
