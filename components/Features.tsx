b8b58b795598a679cd18d7ba9007d1484032607c
Ruben Arcos <ruben.arcos06@gmail.com>
2024-02-25 01:47:55 -0600

    finished the features section.

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,8 +6,8 @@ const Features = () => {
   return (
     // overflow-hidden to avoid scrollbar
     // bg-feature-bg to apply background img
-    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-end">
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -21,7 +21,7 @@ const Features = () => {
          * flex-col - so they appear one on top of the other on mobile devices
          * features section will take 60% on large devices
          */}
-        <div className="-z-20 flex w-full flex-col lg:w-[60%]">
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
           <div className="relative">
             <Image
               src="/camp.svg"
@@ -32,7 +32,7 @@ const Features = () => {
             />
             <h2 className="capitalize bold-40 lg:bold-64">our features</h2>
           </div>
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
             {FEATURES.map((feature) => (
               <FeatureItem
                 key={feature.title}
@@ -61,7 +61,7 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
       <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
-      ,
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">{description}</p>
     </li>
   );
 };
